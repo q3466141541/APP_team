433cbc8ac85484cdc1f551928b7f456116ea5d19
laidongyi <695163222@qq.com>
2019-01-04 05:36:15 +0800

    Add files via upload

---
+++ files/cms_prd/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q],r,_(s,t,u,v,w,x,y,_(),z,_(A,B,C,D,E,_(F,G,H,I),J,null,K,D,L,D,M,N,O,null,P,Q,R,S,T,U,V,Q),W,_(),X,_(),Y,_(Z,[])),ba,_(),bb,_());}; 
+var b="url",c="cms_prd.html",d="generationDate",e=new Date(1546550864203.23),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="time",n="progress_bar",o="TrackY",p="LoadCount",q="OnLoadVariable",r="page",s="packageId",t="6d1207d7e6cb42188c65dfc4c66dab76",u="type",v="Axure:Page",w="name",x="CMS_PRD",y="notes",z="style",A="baseStyle",B="627587b6038d43cca051c114ac41ad32",C="pageAlignment",D="near",E="fill",F="fillType",G="solid",H="color",I=0xFFFFFFFF,J="image",K="imageHorizontalAlignment",L="imageVerticalAlignment",M="imageRepeat",N="auto",O="favicon",P="sketchFactor",Q="0",R="colorStyle",S="appliedColor",T="fontName",U="Applied Font",V="borderWidth",W="adaptiveStyles",X="interactionMap",Y="diagram",Z="objects",ba="masters",bb="objectPaths";
+return _creator();
+})());
---
+++ files/前端_prd/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q],r,_(s,t,u,v,w,x,y,_(),z,_(A,B,C,D,E,_(F,G,H,I),J,null,K,D,L,D,M,N,O,null,P,Q,R,S,T,U,V,Q),W,_(),X,_(),Y,_(Z,[])),ba,_(),bb,_());}; 
+var b="url",c="前端_prd.html",d="generationDate",e=new Date(1546550864045.65),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="time",n="progress_bar",o="TrackY",p="LoadCount",q="OnLoadVariable",r="page",s="packageId",t="972c9421f6914a3eb28971364ce2bc72",u="type",v="Axure:Page",w="name",x="前端_PRD",y="notes",z="style",A="baseStyle",B="627587b6038d43cca051c114ac41ad32",C="pageAlignment",D="near",E="fill",F="fillType",G="solid",H="color",I=0xFFFFFFFF,J="image",K="imageHorizontalAlignment",L="imageVerticalAlignment",M="imageRepeat",N="auto",O="favicon",P="sketchFactor",Q="0",R="colorStyle",S="appliedColor",T="fontName",U="Applied Font",V="borderWidth",W="adaptiveStyles",X="interactionMap",Y="diagram",Z="objects",ba="masters",bb="objectPaths";
+return _creator();
+})());
